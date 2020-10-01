b62c243a76c8711dfd49f4657827565a5eeab123
AKaGaMI-GitHub <netsahabat08@gmail.com>
2020-10-01 21:24:32 +0800

    project 30 sept 2020

---
+++ script.js
@@ -0,0 +1,19 @@
+function validasiform(){
+    if (document.formsar.nama.value == ""){
+        document.getElementById("err_nama").innerHTML = "Cannot be empty"
+    } else {
+        document.getElementById("err_nama").innerHTML = "";
+    }
+    if (document.formsar.email.value == ""){
+        document.getElementById("err_email").innerHTML = "Cannot be empty";
+    } else {
+        document.getElementById("err_email").innerHTML = "";
+    }
+    if (document.formsar.saran.value == ""){
+        document.getElementById("err_saran").innerHTML = "Cannot be empty";
+        
+    } else {
+        document.getElementById("err_saran").innerHTML = "";
+    }
+    return false;
+}
